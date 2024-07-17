7fd19240159636bd44eae7a041ae5654f84bef4e
Luis Melgarejo Narcizo <lmelgare94@gmail.com>
2024-07-17 10:53:50 -0500

    add Category entity

--- src/main/java/com/ironman/Main.java
+++ src/main/java/com/ironman/Main.java
@@ -1,7 +1,21 @@
 package com.ironman;
 
+import com.ironman.entity.Category;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Category category = new Category();
+        category.setId(1L);
+        category.setName("Category 1");
+        category.setDescription("Description 1");
+        category.setUrlKey("URL Key 1");
+        category.setState("A");
+
+        System.out.println("Category Id: " + category.getDescription());
+
+        Category category2 = new Category(2L, "Category 2", "Description 2", "URL Key 2", "B", null, null);
+        System.out.println("Category 2 description: " + category2.getDescription());
     }
 }
