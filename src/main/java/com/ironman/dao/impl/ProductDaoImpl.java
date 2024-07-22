e9dfd86447fae902d3d3ff0b75320c3dcf1fe4ca
Luis Melgarejo Narcizo <lmelgare94@gmail.com>
2024-07-21 23:10:11 -0500

    add product

---
+++ src/main/java/com/ironman/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,35 @@
+package com.ironman.dao.impl;
+
+import com.ironman.dao.ProductDao;
+import com.ironman.entity.Product;
+
+import java.util.List;
+
+public class ProductDaoImpl implements ProductDao {
+
+    @Override
+    public List<Product> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Product findById(Long aLong) throws Exception {
+        return null;
+    }
+
+    @Override
+    public int create(Product entity) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int update(Long aLong, Product entity) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) throws Exception {
+
+    }
+
+}
